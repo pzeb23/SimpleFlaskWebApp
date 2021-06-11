1550c3e908217395698e9b94a90b588e7225b2ba
pzeb23 <pzeb23@gmail.com>
2021-06-11 14:33:28 +0200

    cos1

--- app/main.py
+++ app/main.py
@@ -12,15 +12,30 @@ news_content = ""
 news_image = ""
 
 @app.route("/") 
-def home():
+def home(): 
     return render_template('subfolder/page.html', news_response=news_response, news_content = news_content, news_image = news_image)
+
+@app.route("/getposts", methods=["GET"])
+def get_posts():
+    r = requests.get('https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty')
+    jsonData = r.json()
+
+    result = json.dumps(jsonData[:5])
+    return result
+
+@app.route("/getnews", methods=["GET"])
+def get_news():
+    result = top_headlines["articles"]
+
+    return json.dumps(result)
+
 
 @app.route("/getnewsparam", methods=["POST"])
 def get_newsparam():
 
-    #query = request.form["query"]
+    query = request.form["query"]
 
-    top_headlines = newsapi.get_top_headlines(q="microsoft", language='en')
+    top_headlines = newsapi.get_top_headlines(q=query, language='en')
 
     articles = top_headlines['articles']
     global news_response
@@ -37,8 +52,7 @@ def get_newsparam():
         news_content = articles[0]['content']
         news_image = articles[0]['urlToImage']
 
-    #return redirect(request.referrer)
-    return 2
+    return redirect(request.referrer)
 
 
 #app.run(debug = True)
