382b3861837ca9fc0b78c00ef4579c02eb3ff275
pzeb23 <pzeb23@gmail.com>
2021-06-11 14:02:01 +0200

    cos1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request
-#import requests
+import requests
 import json
-from newsapi import NewsApiClient
+#from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
