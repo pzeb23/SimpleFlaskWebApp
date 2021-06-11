b39722af26e371c71501df16ab9f79adbca8f415
pzeb23 <pzeb23@gmail.com>
2021-06-11 12:30:39 +0200

    message

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect, request
 #import requests
 import json
-from newsapi.newsapi_client import NewsApiClient
+#from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
 # Init
-newsapi = NewsApiClient(api_key='b251fd71f49b4976a8d549c9d51d9080')
+#newsapi = NewsApiClient(api_key='b251fd71f49b4976a8d549c9d51d9080')
 
 @app.route("/")
 def home_view():
